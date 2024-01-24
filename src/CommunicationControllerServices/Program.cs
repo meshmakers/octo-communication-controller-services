52f275158d36b267b55e9cca958c727e73571162
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-24 15:12:42 +0100

    Updated submodules and nuget packages

--- src/CommunicationControllerServices/Program.cs
+++ src/CommunicationControllerServices/Program.cs
@@ -111,7 +111,7 @@ try
 
     var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+    // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
       //  app.UseDeveloperExceptionPage();
