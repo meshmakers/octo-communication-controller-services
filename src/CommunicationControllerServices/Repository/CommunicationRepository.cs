ac21590d04a726994f9b91e8f8739b6352e8c160
GeraldLochner <zerocool@ianitor.net>
2024-03-10 21:30:26 +0100

    AB#1155: New: Updated submodule; fixed namespaces of generated ck model classes

--- src/CommunicationControllerServices/Repository/CommunicationRepository.cs
+++ src/CommunicationControllerServices/Repository/CommunicationRepository.cs
@@ -1,6 +1,6 @@
 using Meshmakers.Octo.ConstructionKit.Contracts;
-using Meshmakers.Octo.ConstructionKit.Models.System.Communication.ConstructionKit.Generated.System.Communication.v1;
-using Meshmakers.Octo.ConstructionKit.Models.System.ConstructionKit.Generated.System.v1;
+using Meshmakers.Octo.ConstructionKit.Models.System.Communication.Generated.System.Communication.v1;
+using Meshmakers.Octo.ConstructionKit.Models.System.Generated.System.v1;
 using Meshmakers.Octo.Runtime.Contracts;
 using Meshmakers.Octo.Runtime.Contracts.MongoDb;
 using Meshmakers.Octo.Runtime.Contracts.Repositories.Query;
