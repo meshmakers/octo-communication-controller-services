73142357e7e3e1565cc8d3a94e5fb8f2085e2d9c
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-03 08:46:38 +0200

    AB#1460: Fix: Debug level when local debugging

--- src/CommunicationControllerServices/Options/CommunicationControllerOptions.cs
+++ src/CommunicationControllerServices/Options/CommunicationControllerOptions.cs
@@ -13,46 +13,50 @@ public class CommunicationControllerOptions
     public CommunicationControllerOptions()
     {
         Authority = "https://localhost:5003";
-        
+
         BrokerHost = "localhost";
         BrokerVirtualHost = "/";
         BrokerPort = 5672;
         BrokerUser = "guest";
         BrokerPassword = "guest";
+#if DEBUG || DEBUGL
+        MinLogLevel = LogLevelDto.Debug;
+#else
         MinLogLevel = LogLevelDto.Warn;
+#endif
     }
 
     /// <summary>
     ///     (Public) base address of the CAS (Central Authorization Services)
     /// </summary>
     public string Authority { get; set; }
-    
-    
+
+
     /// <summary>
     /// Gets or sets the RabbitMQ broker host name
     /// </summary>
     public string BrokerHost { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the RabbitMQ broker virtual host
     /// </summary>
     public string BrokerVirtualHost { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the RabbitMQ broker port
     /// </summary>
     public ushort BrokerPort { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the RabbitMQ broker username
     /// </summary>
     public string? BrokerUser { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the RabbitMQ broker password
     /// </summary>
     public string? BrokerPassword { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the minimal log level to be logged
     /// </summary>
