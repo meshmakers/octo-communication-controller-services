7ede5c613e9d41b94e5fa483dd1933a78ebe00d5
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-12 17:37:55 +0200

    #AB1463: New: Execute pipelines using REST

--- src/CommunicationControllerServices/Services/ITriggerManagementService.cs
+++ src/CommunicationControllerServices/Services/ITriggerManagementService.cs
@@ -1,3 +1,5 @@
+using Meshmakers.Octo.ConstructionKit.Contracts;
+
 namespace Meshmakers.Octo.Backend.CommunicationControllerServices.Services;
 
 /// <summary>
@@ -6,9 +8,18 @@ namespace Meshmakers.Octo.Backend.CommunicationControllerServices.Services;
 public interface ITriggerManagementService
 {
     /// <summary>
+    /// Execute the pipeline for the tenant
+    /// </summary>
+    /// <param name="tenantId">The tenant id</param>
+    /// <param name="meshPipelineRtEntityId">The pipeline entity object id</param>
+    /// <param name="pipelineInput">The input for the pipeline</param>
+    /// <returns>The output of the pipeline</returns>
+    Task<string?> ExecutePipelineAsync(string tenantId, RtEntityId meshPipelineRtEntityId, string? pipelineInput);
+    
+    /// <summary>
     /// Remove the schedule for the triggers of the tenant
     /// </summary>
-    /// <param name="tenantId"></param>
+    /// <param name="tenantId">The tenant id</param>
     /// <returns></returns>
     Task RemoveScheduleAsync(string tenantId);
     
